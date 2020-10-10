0cf30904812df3a497dfacb064b18556e0388c8e
Adam Rashid <rashid.uj@gmail.com>
2020-10-11 01:49:24 +0800

    Added Add functionality for income and expenses

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,27 @@
+// Add functionalities for both Income and Expense
+const addIncomeButton = document.getElementById("btn-add-income");
+const addExpensesButton = document.getElementById("btn-add-expenses");
+
+addIncomeButton.addEventListener("click", addIncomeNode);
+addExpensesButton.addEventListener("click", addExpenseNode);
+
+//TODO: Compile into one function
+function addIncomeNode(e) {
+    let newNode = e.target.previousElementSibling.cloneNode(true);
+    document.getElementById("income-list").insertBefore(newNode, addIncomeButton);
+}
+
+function addExpenseNode(e) {
+    let newNode = e.target.previousElementSibling.cloneNode(true);
+    document.getElementById("expenses-list").insertBefore(newNode, addExpensesButton);
+}
+
+// Process Sankey
 const generateButton = document.getElementById("generate-button");
 generateButton.addEventListener("click", processSankey)
 
 function processSankey(e) {
-    // Obtain input from all fields
+    // Get Income and Expense
+    document.getElementsByClassName("item-expenses");
+    document.getElementsByClassName("item-income");
 }
