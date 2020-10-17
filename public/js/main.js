c162561ee8857f658677e4d8d28b91da659d56c8
Adam Rashid <rashid.uj@gmail.com>
2020-10-17 23:10:57 +0800

    Added close modal and changed to list

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,46 @@
 import {process_sankey} from "./sankeymatic.js";
 
-// Add functionalities for both Income and Expense
-const addIncomeButton = document.getElementById("btn-add-income");
-const addExpensesButton = document.getElementById("btn-add-expenses");
+let nodeList = [{
+    "type": "income",
+    "amount": 5000,
+    "category": "salary",
+    "parentCategory": ""
+}];
 
-addIncomeButton.addEventListener("click", addIncomeNode);
-addExpensesButton.addEventListener("click", addExpenseNode);
+// Add functionalities for both Income and Expense
+const addIncomeButton = document.getElementById("button-add-income");
+const addExpensesButton = document.getElementById("button-add-expenses");
+
+addIncomeButton.addEventListener("click", triggerModal);
+addExpensesButton.addEventListener("click", triggerModal);
 
 //TODO: Compile into one function
-function addIncomeNode(e) {
-    let newNode = e.target.previousElementSibling.cloneNode(true);
-    document.getElementById("income-list").insertBefore(newNode, addIncomeButton);
+function triggerModal(e) {
+    if (e.target.id = "button-add-income") {
+        document.getElementById("modal-income").classList.toggle("is-active");
+    } else if (e.target.id = "button-add-expense") {
+        // TODO: Open modal-expense
+    } 
 }
 
 function addExpenseNode(e) {
     let newNode = e.target.previousElementSibling.cloneNode(true);
     document.getElementById("expenses-list").insertBefore(newNode, addExpensesButton);
+}
+
+// Close modal
+const closePopupButtons = [...document.getElementsByClassName("modal-close")];
+
+closePopupButtons.forEach((val, ind, arr) => {
+    val.addEventListener("click", closeModal);
+});
+
+function closeModal(e) {
+    if (e.target.parentElement.id == "modal-income") {
+        e.target.parentElement.classList.toggle("is-active");
+    } else if (e.target.parentElement == "modal-expense") {
+        // TODO: Close modal-expense
+    }
 }
 
 // Process Sankey
