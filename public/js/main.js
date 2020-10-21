949a967084b2b267c3df3b61103c756065ab8cb0
Adam Rashid <rashid.uj@gmail.com>
2020-10-21 19:09:57 +0800

    Added adding to list functionality

--- public/js/main.js
+++ public/js/main.js
@@ -70,44 +70,48 @@ let categoryList = [
 // Load data into lists
 function loadListData(nodeList) {
   nodeList.forEach((val, ind, arr) => {
-    const tr = document.createElement("tr");
-    const th = document.createElement("th");
-    let td = document.createElement("td");
-
-    // Add index number, amount, category and parent category to table
-    th.appendChild(document.createTextNode(ind));
-    tr.appendChild(th);
-
-    td.appendChild(document.createTextNode(val["amount"]));
-    tr.appendChild(td);
-
-    td = document.createElement("td");
-    td.appendChild(document.createTextNode(val["categoryID"]));
-    tr.appendChild(td);
-
-    td = document.createElement("td");
-    td.appendChild(document.createTextNode(val["parentCategoryID"]));
-    tr.appendChild(td);
-
-    // Add button in action column
-    td = document.createElement("td");
-    const button = document.createElement("button");
-    button.className = "button is-danger is-outlined is-small";
-    button.type = "button";
-    button.appendChild(document.createTextNode("Delete"));
-    td.appendChild(button);
-    tr.appendChild(td);
-
-    // Choose depending on type to appear in the income or expenses table
-    if (val["type"] == "income") {
-      document.querySelector("#income-list tbody").appendChild(tr);
-    }
-    if (val["type"] == "expense") {
-      document.querySelector("#expenses-list tbody").appendChild(tr);
-    }
+    addDataIntoTable(val, ind);
   });
 }
 
+// Add the
+function addDataIntoTable(data, ind) {
+  const tr = document.createElement("tr");
+  const th = document.createElement("th");
+  let td = document.createElement("td");
+
+  // Add index number, amount, category and parent category to table
+  th.appendChild(document.createTextNode(ind));
+  tr.appendChild(th);
+
+  td.appendChild(document.createTextNode(data["amount"]));
+  tr.appendChild(td);
+
+  td = document.createElement("td");
+  td.appendChild(document.createTextNode(data["categoryID"]));
+  tr.appendChild(td);
+
+  td = document.createElement("td");
+  td.appendChild(document.createTextNode(data["parentCategoryID"]));
+  tr.appendChild(td);
+
+  // Add button in action column
+  td = document.createElement("td");
+  const button = document.createElement("button");
+  button.className = "button is-danger is-outlined is-small";
+  button.type = "button";
+  button.appendChild(document.createTextNode("Delete"));
+  td.appendChild(button);
+  tr.appendChild(td);
+
+  // Choose depending on type to appear in the income or expenses table
+  if (data["type"] == "income") {
+    document.querySelector("#income-list tbody").appendChild(tr);
+  }
+  if (data["type"] == "expense") {
+    document.querySelector("#expenses-list tbody").appendChild(tr);
+  }
+}
 // Call setup functions upon window load
 window.onload = function () {
   loadListData(nodeList);
@@ -122,7 +126,6 @@ addExpensesButton.addEventListener("click", triggerModal);
 
 // Function to trigger the modal and fill the correct
 function triggerModal(e) {
-  console.log(e.target.id);
   if (e.target.id == "button-add-income") {
     document.getElementById("modal-income").classList.toggle("is-active");
     loadModalListData("income");
@@ -132,30 +135,28 @@ function triggerModal(e) {
   }
 }
 
-// Function to Modal List
+// Function to Modal List Dropdown
 function loadModalListData(type) {
   if (type === "income") {
-    let parent = document.getElementById("parent-select-income-1");
-    let category = document.getElementById("category-select-income-1");
+    let parent = document.getElementById("select-parent-income");
 
     categoryList.forEach((val, ind, arr) => {
       if (val["type"] === "income") {
         let option = document.createElement("option");
 
         option.appendChild(document.createTextNode(val["categoryName"]));
-        category.appendChild(option);
+        parent.appendChild(option);
       }
     });
   } else if (type === "expense") {
-    let parent = document.getElementById("parent-select-expenses-1");
-    let category = document.getElementById("category-select-expenses-1");
+    let parent = document.getElementById("select-parent-expenses");
 
     categoryList.forEach((val, ind, arr) => {
       if (val["type"] === "expense") {
         let option = document.createElement("option");
 
         option.appendChild(document.createTextNode(val["categoryName"]));
-        category.appendChild(option);
+        parent.appendChild(option);
       }
     });
   }
@@ -168,13 +169,66 @@ closePopupButtons.forEach((val, ind, arr) => {
   val.addEventListener("click", closeModal);
 });
 
+// Function to closeModal. Can be triggered from the click event and also manually by passing in the value as manual
 function closeModal(e) {
-  if (
-    e.target.parentElement.id == "modal-income" ||
-    e.target.parentElement.id == "modal-expense"
-  ) {
-    e.target.parentElement.classList.toggle("is-active");
+  if (typeof e.target !== "undefined") {
+    if (
+      e.target.parentElement.id === "modal-income" ||
+      e.target.parentElement.id === "modal-expense"
+    ) {
+      e.target.parentElement.classList.toggle("is-active");
+    }
+  } else {
+    if (e === "income") {
+      document.getElementById("modal-income").classList.toggle("is-active");
+    } else if (e === "expense") {
+      document.getElementById("modal-expense").classList.toggle("is-active");
+    }
   }
+}
+
+// Get reference and attach listeners to buttons in the modal
+const addIncomeButtonModal = document.getElementById("button-add-income-modal");
+const addExpensesButtonModal = document.getElementById(
+  "button-add-expenses-modal"
+);
+
+addIncomeButtonModal.addEventListener("click", addNode);
+addExpensesButtonModal.addEventListener("click", addNode);
+
+// Function to add node and
+function addNode(e) {
+  e.preventDefault();
+  let record;
+
+  if (e.target.id == "button-add-income-modal") {
+    record = {
+      type: "income",
+      amount: document.getElementById("input-income-amount").value,
+      categoryID: document.getElementById("input-income-category").value,
+      parentCategoryID: "None",
+    };
+
+    addDataIntoTable(record, 3);
+    closeModal("income");
+  } else if (e.target.id == "button-add-expenses-modal") {
+    record = {
+      type: "expense",
+      amount: document.getElementById("input-expenses-amount").value,
+      categoryID: document.getElementById("input-expenses-category").value,
+      parentCategoryID: "None",
+    };
+
+    addDataIntoTable(record, 3);
+    closeModal("income");
+  }
+
+  // TODO: Replace with backend API. Only if successful call the toast.
+  nodeList.push(record);
+  
+  // Display Toast Message
+  document.querySelector(".notification").classList.toggle("is-hidden");
+ 
 }
 
 // Process Sankey
@@ -192,7 +246,6 @@ tabButtons.forEach((val, ind, arr) => {
 
 function applyIsActive(e) {
   e.preventDefault();
-  console.log(e.target);
   e.target.parentElement.classList.toggle("is-active");
 
   if (e.target.id == "budget-input-option") {
