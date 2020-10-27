4eaff11d2a8735f7068538af1041a27e85ec9f2d
Adam Rashid <rashid.uj@gmail.com>
2020-10-27 17:25:07 +0800

    Defined database schema and connection params

--- database/setup.sql
+++ database/setup.sql
@@ -1,1 +1,18 @@
+-- Command to create the Database
 CREATE DATABASE budgetapp;
+
+-- Command to create the Records table
+CREATE TABLE record(
+    record_id SERIAL PRIMARY KEY,
+    record_type SMALLINT NOT NULL,
+    amount INT NOT NULL,
+    category_id INT REFERENCES category
+);
+
+-- Command to create the Category table
+CREATE TABLE category(
+    category_id SERIAL PRIMARY KEY,
+    category_type SMALLINT NOT NULL,
+    category_name VARCHAR(255),
+    parent_category_id SMALLINT
+);
